3461963f8b6b13b4542aaa186b4db97aabb3aa48
ヨッシー <ktc23a32f0005@edu.kyoto-tech.ac.jp>
2025-08-23 08:04:01 +0000

    会員登録機能、一旦実装完了

--- public/entry/confirm.php
+++ public/entry/confirm.php
@@ -17,58 +17,58 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 // DB接続
 
-$pdo = db();
+  $pdo = db();
 
 // DB既存データへの重複確認(name)
 
 
-$sql_n = "SELECT COUNT(name) AS count FROM users WHERE name = :name";
+  $sql_n = "SELECT COUNT(name) AS count FROM users WHERE name = :name";
 
-$user_name = h_s($_POST["entry_name"]);
+  $user_name = h_s($_POST["entry_name"]);
 
-$stmt_n = $pdo->prepare($sql_n);
-
-
-$stmt_n->bindParam(":name", $user_name, PDO::PARAM_STR);
-
-$stmt_n->execute();
-
-$n_count = $stmt_n->fetch(PDO::FETCH_ASSOC);
+  $stmt_n = $pdo->prepare($sql_n);
 
 
+  $stmt_n->bindParam(":name", $user_name, PDO::PARAM_STR);
 
-// DB既存データへの重複確認(email)
+  $stmt_n->execute();
 
-
-$sql_e = "SELECT COUNT(email) AS count FROM users WHERE email = :email";
-
-$email = h_s($_POST["entry_email"]);
-
-$stmt_e = $pdo->prepare($sql_e);
+  $n_count = $stmt_n->fetch(PDO::FETCH_ASSOC);
 
 
-$stmt_e->bindParam(":email", $email, PDO::PARAM_STR);
 
-$stmt_e->execute();
-
-$e_count = $stmt_e->fetch(PDO::FETCH_ASSOC);
+  // DB既存データへの重複確認(email)
 
 
-if ($e_count["count"] < 1 && $n_count["count"] < 1) {
-  header("Location: pre.php");
+  $sql_e = "SELECT COUNT(email) AS count FROM users WHERE email = :email";
+
+  $email = h_s($_POST["entry_email"]);
+
+  $stmt_e = $pdo->prepare($sql_e);
+
+
+  $stmt_e->bindParam(":email", $email, PDO::PARAM_STR);
+
+  $stmt_e->execute();
+
+  $e_count = $stmt_e->fetch(PDO::FETCH_ASSOC);
+
+
+  if ($e_count["count"] < 1 && $n_count["count"] < 1) {
+    header("Location: pre.php");
+    exit;
+
+  } elseif ($n_count["count"] > 0) {
+
+    $_SESSION["user_error"] = "入力されたユーザーネームは、既に登録されています。";
+
+  } else {
+
+    $_SESSION["user_error"] = "入力されたメールアドレスは、既に登録されています。";
+
+  }
+  header("Location: form.php");
   exit;
-
-} elseif ($n_count["count"] > 0) {
-
-  $_SESSION["user_error"] = "入力されたユーザーネームは、既に登録されています。";
-
-} else {
-
-  $_SESSION["user_error"] = "入力されたメールアドレスは、既に登録されています。";
-
-}
-header("Location: form.php");
-exit;
 
 
 }
