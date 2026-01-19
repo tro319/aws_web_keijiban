17ff93756250e212403f7edbaf53cb8e98633bb7
よたろ <ktc23a32f0005@edu.kyoto-tech.ac.jp>
2026-01-19 10:23:53 +0000

    a

--- public/kouki/20251112/profile_edit.php
+++ public/kouki/20251112/profile_edit.php
@@ -224,7 +224,7 @@ document.querySelector("button[type='submit']").addEventListener("click", async(
   ctx.drawImage(bitmap, 0, 0, w, h);
 
 
-  canvas.toBlob((blob) => {
+  canvas.toBlob(ansync (blob) => {
     
     const formData = new FormData();
 
@@ -238,11 +238,9 @@ document.querySelector("button[type='submit']").addEventListener("click", async(
       method: "POST",
       body: formData
 
-    }).then(() => {
-      
-      window.location.href = "profile_edit.php";
-
     });
+
+    window.location.href = "profile_edit.php";
 
   }, "image/png", 0.9);
 
