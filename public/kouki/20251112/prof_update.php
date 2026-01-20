5d7f68ec7a2ae050bf0084009353b75e65d3fe8b
tro319 <149455937+tro319@users.noreply.github.com>
2026-01-21 00:10:04 +0900

    Update prof_update.php

--- public/kouki/20251112/prof_update.php
+++ public/kouki/20251112/prof_update.php
@@ -352,35 +352,21 @@ $get_result = $get_stmt->fetchAll();
 
 		ctx.drawImage(bitmap, 0, 0, w, h);
 
-		resizedBlob = await new Promise((resolve) => 
-			canvas.toBlob((blob) => resolve(blob), "image/png", 0.9)
+	    canvas.toBlob((blob) => {
+			const fileInput = document.getElementById("image_input");
 
-		);
+			const newFile = new File([blob], "upload.png", { type: "image/png" });
 
-	});
+			const dataTransfer = new DataTransfer();
 
-	const form = document.querySelector("form");
+			dataTransfer.items.add(newFile);
 
-	form.addEventListener("submit", async (e) => {
+			fileInput.files = dataTransfer.files;
 
-		e.preventDefault();
-
-		const formData = new FormData(form);
-
-		if (resizedBlob) {
-
-			formData.set("image_file", resizedBlob, "upload.png");
-
-		}
-
-		await fetch("prof_update.php", {
-			method: "POST",
-			body: formData
-		});
-
-		window.location.href="prof_update.php";
+		}, "image/png", 0.9);
 
 	});
+
 
 </script>
 
