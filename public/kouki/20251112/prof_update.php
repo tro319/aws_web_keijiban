07cb9ce5e5dbb7c522fb4b6c74e3bf6d2441d6ec
tro319 <149455937+tro319@users.noreply.github.com>
2026-01-20 23:42:56 +0900

    Update prof_update.php

--- public/kouki/20251112/prof_update.php
+++ public/kouki/20251112/prof_update.php
@@ -308,7 +308,7 @@ $get_result = $get_stmt->fetchAll();
 
 	let resizedBlob = null;
 
-	document.getElementById("image_input").addEventListener("change", function(e) {
+	document.getElementById("image_input").addEventListener("change", async(e) {
 
 		const file = e.target.files[0];
 
