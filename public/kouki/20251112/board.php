c86c3a35a589e7f552ae5a572d8260c012ee45da
tro319 <149455937+tro319@users.noreply.github.com>
2026-01-24 02:48:53 +0900

    Update board.php

--- public/kouki/20251112/board.php
+++ public/kouki/20251112/board.php
@@ -62,11 +62,13 @@ require("./header.php");
 
         <!-- 投稿内容 -->
 
-        <?php if ($post["pic_name1"] != null): ?>
-
-            <div class="images-cover post-images-cover">
+                  <div class="images-cover post-images-cover">
     
                 <ul class="images post-images">
+
+        <?php if ($post["pic_name1"] != null): ?>
+
+
 
                     <li>
 
@@ -80,11 +82,13 @@ require("./header.php");
             
                     </li>
 
-                </ul>
 
-            </div>
 
         <?php endif; ?>
+
+                                </ul>
+
+            </div>
     
         
         <p>投稿文: <?= nl2br(htmlspecialchars($post["content"])) ?></p>
